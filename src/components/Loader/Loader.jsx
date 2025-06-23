c61ad1b25bc92f5fec087dd5ccc2e571ff3eaa7c
EssiJunior <nessipjunior@gmail.com>
2025-06-23 05:18:23 +0100

    Done splash screen

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,24 +1,18 @@
 // REACT IMPORTS
-import React from 'react'
-import './loader.css'
+import React from "react";
+import "./loader.css";
 
-const Loader = ({size = '54px', marginTop = '2rem', bg='#47abcd'}) => {
-    return (
-        <div class="loader" style={{width:size, height:size, marginTop:marginTop }}>
-            <div class="bar1" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-            <div class="bar2" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-            <div class="bar3" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-            <div class="bar4" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-            <div class="bar5" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-            <div class="bar6" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-            <div class="bar7" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-            <div class="bar8" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-            <div class="bar9" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-            <div class="bar10" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-            <div class="bar11" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-            <div class="bar12" style={{background:bg, boxShadow:`0 0 3px ${bg}`}}></div>
-        </div>
-    )
-}
+const Loader = () => {
+  return (
+    <div className="loader relative w-[54px] h-[54px] mt-10 rounded-[10px]">
+      {Array.from({ length: 12 }).map((_, index) => (
+        <div
+          key={index}
+          className={`bar${index + 1} bg-primary shadow-[0_0_3px_#ff9900]`}
+        ></div>
+      ))}
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
