ceb2b1b42ad601e06b4398c26ab296515dda85f4
EssiJunior <nessipjunior@gmail.com>
2025-06-24 16:06:18 +0100

    Done web view of home page

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,8 +4,20 @@ import logo from "./logo/logo.png"
 import calculation from "./lotties/calculation.json"
 import calculate from "./lotties/calculate.json"
 
+// ICONS
+import calculator from "./icons/calculator.png"
+import calculatorWhite from "./icons/calculator-white.png"
+import calculatorBlack from "./icons/calculator-black.png"
+import bulb from "./icons/bulb.png"
+import copy from "./icons/copy.png"
+
 export {
     logo,
     calculation,
-    calculate
+    calculate,
+    calculator,
+    calculatorWhite,
+    calculatorBlack,
+    bulb,
+    copy
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,15 @@ import { Link, useNavigate } from "react-router-dom";
 // COMPONENTS AND UTILITIES
 import withSplashScreen from "../hoc/withSplashScreen";
 import { defaultOptions } from "../utils";
-import Button from "../components/Button/Button";
+import Button from "../components/Button";
 
 // ASSETS
-import { calculate } from "../assets";
+import {
+  calculate,
+  calculator,
+  calculatorWhite,
+  calculatorBlack,
+} from "../assets";
 
 // STYLES
 import "../styles/home.css";
@@ -17,25 +22,31 @@ function Home() {
   const navigate = useNavigate();
 
   return (
-    <main className="home h-dvh w-full relative grid lg:grid-rows-1  grid-cols-1 lg:grid-cols-[1.5fr_2fr] bg-white text-black">
+    <main className="home h-dvh w-full grid lg:grid-rows-1 grid-cols-1 lg:grid-cols-[1.5fr_2fr] bg-white text-black relative overflow-hidden">
       <div className="p-5 flex flex-col justify-center items-start gap-10 order-2 lg:order-1">
         <p>
           Hey <span className="salute-emoji">🫡</span>
         </p>
         <p className="">
-          You're on <span className="font-black md:text-[45px] sm:text-[35px] xs:text-[25px] gradient__text">Scoreify</span>
+          You're on{" "}
+          <span className="font-black md:text-[45px] sm:text-[35px] xs:text-[25px] gradient__text">
+            Scoreify
+          </span>
         </p>
         <div className="divider"></div>
         <p>
-          You want to be sure of your average ? You want to determine it before
+          You want to be sure of <span className="gradient__text">your average</span> ? You want to determine it before
           your school does ?
         </p>
         <p>
           Scoreify does it for you. <span className="emoji">😎</span>
         </p>
         <div className="flex gap-4">
-          <Button text="Get started" handleClick={() => navigate('/management')}/>
-          <Button text="Sign In" handleClick={() => navigate('/signin')} />
+          <Button
+            text="Get started"
+            handleClick={() => navigate("/management")}
+          />
+          <Button text="Sign In" handleClick={() => navigate("/signin")} />
         </div>
       </div>
       <div className="flex justify-center items-center bg-secondary/20 order-1 lg:order-2">
@@ -45,6 +56,25 @@ function Home() {
           height={"70%"}
           width={"70%"}
         />
+      </div>
+
+      <div className="absolute -top-1/4 -left-1/6 w-2/3 aspect-square border border-secondary/80 rounded-full ">
+        <div className="orbit relative h-full rounded-full ">
+          <img
+            src={calculatorBlack}
+            alt="bulb"
+            className="absolute right-0 bottom-[240px] w-[85px]"
+          />
+        </div>
+      </div>
+      <div className=" absolute -top-[40px] -left-[75px] w-1/2 aspect-square border border-primary rounded-full">
+        <div className="orbit relative h-full rounded-full ">
+          <img
+            src={calculator}
+            alt="calculator"
+            className="absolute right-[50px] top-[100px] w-[75px]"
+          />
+        </div>
       </div>
     </main>
   );
