c9d33d9be7a39987c339ce50d0ce9f5f5a2104ba
EssiJunior <nessipjunior@gmail.com>
2026-02-20 23:03:48 +0100

    Added vite best practices configs

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: [{
+            find: "@",
+            replacement: path.resolve(__dirname, "src")
+        }],
+    },
+    optimizeDeps: {
+        exclude: ['js-big-decimal']
+    },
+    build: {
+        cssMinify: 'lightningcss',
+        minify: 'esbuild',
+        cssCodeSplit: true,
+    }
 })
