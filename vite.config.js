0487853e24e3df14c3006b2fad3859a0c1d6b707
EssiJunior <nessipjunior@gmail.com>
2026-02-20 23:18:12 +0100

    Corrected config issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import {
 } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import * as path from 'path';
+
 
 // https://vite.dev/config/
 export default defineConfig({
